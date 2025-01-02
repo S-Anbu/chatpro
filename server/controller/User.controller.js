5e8ee78cfa105d8b0502d7d3e07b08e56f03f2a1
Anbarasan Subramani <161632074+S-Anbu@users.noreply.github.com>
2025-01-02 20:51:48 +0530

    Update User.controller.js

--- server/controller/User.controller.js
+++ server/controller/User.controller.js
@@ -112,10 +112,10 @@ const login = async (req, res) => {
     await user.save();
 
     res.cookie("jwt", user.token, {
-      httpOnly: true,
-      maxAge: 60 * 60 * 1000, // 1 hour
-      // maxAge:  60000, // 1 min
-    });
+  httpOnly: true,
+  maxAge: 60 * 60 * 1000, // 1 hour
+  secure: true,           // Required for HTTPS     // For cross-origin requests
+});
     return res.status(200).json({ message: "Login Successfully" });
   } catch (error) {
     console.error(error);
